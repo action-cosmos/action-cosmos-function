5848249daec67e2faa2a56e244ad2c6233d7415e
roharon <roharon98@gmail.com>
2021-12-05 04:07:01 +0900

    Fix: redis_port env

--- client/redis_client.py
+++ client/redis_client.py
@@ -3,6 +3,6 @@ import redis
 
 def redis_client():
     r = redis.Redis(host=os.getenv('redis_host'), socket_timeout=3,
-                    port=os.getenv('redis_post'), db=0,password=os.getenv('redis_password'), ssl=True)
+                    port=os.getenv('redis_port'), db=0,password=os.getenv('redis_password'), ssl=True)
 
     return r
