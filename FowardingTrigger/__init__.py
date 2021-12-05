fd67eb3a078354e50354b1c5cf67102a2f13217e
Kim Seohyun <kshjessica@ewhain.net>
2021-12-05 11:10:34 +0900

    Feat: fowarding trigger redis_client file

--- FowardingTrigger/__init__.py
+++ FowardingTrigger/__init__.py
@@ -1,24 +1,16 @@
+from client.redis_client import redis_client
+import json
 import logging
-
+import requests
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Complete forwarding requested')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    option = req.get_json()['option']
+    message_type = req.get_json()['message_type']
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    if not message_type:
+        return func.HttpResponse("필수 값이 없습니다", status_code=400)
+    
