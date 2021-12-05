27125bc4e91aae377062cac2d7cc8fa698d4149f
roharon <roharon98@gmail.com>
2021-12-05 12:45:54 +0900

    Fix: option trigger redis

--- OptionTrigger/__init__.py
+++ OptionTrigger/__init__.py
@@ -29,8 +29,6 @@ def validate_message_params(user_token, message_type, data):
         slack_webhook_url = data['slack_webhook_url']
         if slack_webhook_url is None:
             raise ValidateError
-        redis_client.set(MESSAGE_TYPE_KEY.format(user_token), slack_webhook_url)
-        redis_client.set(SLACK_WEBHOOK_URL_KEY.format(user_token), slack_webhook_url)
 
     elif message_type == 'kakaowork':
         kakao_app_key = data['app_key']
@@ -45,16 +43,16 @@ def set_message_params(user_token, message_type, data):
     if message_type == 'slack':
         slack_webhook_url = data['slack_webhook_url']
 
-        redis_client.set(MESSAGE_TYPE_KEY.format(user_token), 'slack')
-        redis_client.set(SLACK_WEBHOOK_URL_KEY.format(user_token), slack_webhook_url) 
+        redis_client().set(MESSAGE_TYPE_KEY.format(user_token), 'slack')
+        redis_client().set(SLACK_WEBHOOK_URL_KEY.format(user_token), slack_webhook_url) 
 
     elif message_type == 'kakaowork':
         kakao_app_key = data['app_key']
         kakao_channel_id = data['channel_id']
 
-        redis_client.set(MESSAGE_TYPE_KEY.format(user_token), 'kakaowork')
-        redis_client.set(KAKAO_APP_KEY.format(user_token), kakao_app_key)
-        redis_client.set(KAKAO_CHANNEL_ID_KEY.format(user_token), kakao_channel_id)
+        redis_client().set(MESSAGE_TYPE_KEY.format(user_token), 'kakaowork')
+        redis_client().set(KAKAO_APP_KEY.format(user_token), kakao_app_key)
+        redis_client().set(KAKAO_CHANNEL_ID_KEY.format(user_token), kakao_channel_id)
 
 class ValidateError:
     pass
